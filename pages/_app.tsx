ace0eb83ff0a01ab5bb30f94f958fae2bfa012f7
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-01-14 23:29:47 -0300

    add head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,17 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+
 import { ModalProvider } from "../src/hook/useModal";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <Head>
+        <title>Warren Transações</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="shortcut icon" href="/logo.png" />
+      </Head>
       <ModalProvider>
         <Component {...pageProps} />
       </ModalProvider>
